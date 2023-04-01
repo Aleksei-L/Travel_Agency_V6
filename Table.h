1efb2a5415bdb299675d8aabd2654ef2b479822f
Aleksei Lobanov <lobanov782@gmail.com>
2023-04-01 23:21:15 +0300

    Added v field in table class Edited a lot of functions in Table class Deleted individual functions in Table class

--- Table.h
+++ Table.h
@@ -1,35 +1,25 @@
 #pragma once
-#include "Client.h"
+#include "Vector.h"
 
 class Table {
 private:
-	int size; // Размер таблицы
-	T* m; // Таблица клиентов
+	Vector v; // Таблица клиентов
 	T* current; // Указатель на первый свободный в таблице
 public:
 	Table(int s);
 	~Table();
-	T* Begin();
-	T* End();
-	int Length();
-	int GetSize();
-	T* Erase(T* pos);
-	void Clear();
-	void Resize(int plusSize);
-	T* Insert(const T& newClient);
-	int Input(T item);
-	void Output();
-	void Sort();
-	int Search(const T& tempClient);
-	int Replace(const T& oldClient, const T& newClient);
-	int Remove(const T& badClient);
-	T* GetPntr(int i);
-	int Describe(T* pntr);
-	// Индивидуальные функции
-	T* Insert(T* pos, T* first_item, T* last_item);
-	int Remove(T* first, T* last, T* first_item, T* last_item);
-	T* Search(T* first, T* last, T* first_item, T* last_item);
-	T* Erase(T* first, T* last);
-	int Replace(T* first, T* last, const T& oldClient, const T& newClient);
-	void Sort(int first, int last);
+	T* begin();
+	T* end();
+	int length();
+	int getSize();
+	T* erase(T* pos);
+	void clear();
+	void resize(int plusSize);
+	T* insert(const T& newClient);
+	int input(T item);
+	void output();
+	void sort();
+	int search(const T& tempClient);
+	int replace(const T& oldClient, const T& newClient);
+	int remove(const T& badClient);
 };
